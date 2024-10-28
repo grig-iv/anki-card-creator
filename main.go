a75c72975a4d8701b34679b6e355797b98c55659
grig-iv <abstractgrig@gmail.com>
2024-10-29 00:03:23 +0300

    add bubbletea

--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
-	"github.com/grig-iv/anki-card-creator/ld"
-	// "github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	screen tea.Model
+}
+
+const (
+	logPath = "log"
 )
 
 func main() {
-	page, err := ld.ParseUrl("https://www.ldoceonline.com/dictionary/shackle")
+	f, err := tea.LogToFile(logPath, "")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+
+	}
+	defer f.Close()
+
+	os.Truncate(logPath, 0)
+
+	p := tea.NewProgram(initModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initModel() model {
+	return model{
+		screen: newSearchScreen(),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.screen.Init()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		}
+	case error:
+		log.Println(msg)
+		return m, nil
 	}
 
-	fmt.Println(page)
+	m.screen, cmd = m.screen.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return m.screen.View()
 }
