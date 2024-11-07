614017e55810064a57909b93386f453d2a2bae92
grig-iv <abstractgrig@gmail.com>
2024-11-08 01:05:34 +0300

    add anki connect

---
+++ anki/add_note.go
@@ -0,0 +1,29 @@
+package anki
+
+type addNoteParams struct {
+	DeckName  string            `json:"deckName"`
+	ModelName string            `json:"modelName"`
+	Fields    map[string]string `json:"fields"`
+	Options   AddNoteOptions    `json:"options"`
+	Tags      []string          `json:"tags"`
+	Audio     []Media           `json:"audio"`
+	Video     []Media           `json:"video"`
+	Picture   []Media           `json:"picture"`
+}
+
+type Media struct {
+	Url      string   `json:"url"`
+	Filename string   `json:"filename"`
+	SkipHash string   `json:"skipHash"`
+	Fields   []string `json:"fields"`
+}
+
+type AddNoteOptions struct {
+	AllowDuplicate        bool   `json:"allowDuplicate"`
+	DuplicateScope        string `json:"duplicateScope"`
+	DuplicateScopeOptions struct {
+		DeckName       string `json:"deckName"`
+		CheckChildren  bool   `json:"checkChildren"`
+		CheckAllModels bool   `json:"checkAllModels"`
+	} `json:"duplicateScopeOptions"`
+}
---
+++ anki/anki.go
@@ -0,0 +1,55 @@
+package anki
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type Request struct {
+	Action  string      `json:"action"`
+	Version int         `json:"version"`
+	Params  interface{} `json:"params,omitempty"`
+}
+
+type Response struct {
+	Result interface{} `json:"result"`
+	Error  string      `json:"error"`
+}
+
+func SendRequest(name string, params interface{}) (*Response, error) {
+	request := Request{
+		Action:  name,
+		Version: 6,
+		Params:  params,
+	}
+
+	requestStr, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post("http://localhost:8765", "application/json", bytes.NewReader(requestStr))
+	if err != nil {
+		return nil, err
+	}
+
+	response := &Response{}
+	responseStr, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(responseStr), response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Error != "" {
+		return nil, errors.New(response.Error)
+	}
+
+	return response, nil
+}
