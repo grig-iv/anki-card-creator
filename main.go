5b9e6d7b9358007c1a234a1a801fd543dc1f7076
grig-iv <abstractgrig@gmail.com>
2024-11-09 13:07:48 +0300

    move model to separate file

--- main.go
+++ main.go
@@ -5,12 +5,7 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/grig-iv/anki-card-creator/ld"
 )
-
-type model struct {
-	screen tea.Model
-}
 
 const (
 	logPath = "log"
@@ -30,52 +25,4 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func newModel() model {
-	return model{
-		screen: startupScreen{},
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return m.screen.Init()
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case openSearchMsg:
-		m.screen = newSearchScreen()
-		return m, m.screen.Init()
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-		}
-	case error:
-		log.Println(msg)
-		return m, nil
-	case pageMsg:
-		switch page := msg.page.(type) {
-		case ld.WordPage:
-			if _, ok := m.screen.(wordScreen); !ok {
-				m.screen = newWordScreen(page)
-				return m, m.screen.Init()
-			}
-		case ld.SearchPage:
-			if _, ok := m.screen.(searchScreen); !ok {
-				m.screen = newSearchScreen()
-				return m, m.screen.Init()
-			}
-		}
-	}
-
-	m.screen, cmd = m.screen.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return m.screen.View()
 }
---
+++ model.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/grig-iv/anki-card-creator/ld"
+)
+
+type model struct {
+	screen tea.Model
+}
+
+func newModel() model {
+	return model{
+		screen: startupScreen{},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return m.screen.Init()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case openSearchMsg:
+		m.screen = newSearchScreen()
+		return m, m.screen.Init()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		}
+	case error:
+		log.Println(msg)
+		return m, nil
+	case pageMsg:
+		switch page := msg.page.(type) {
+		case ld.WordPage:
+			if _, ok := m.screen.(wordScreen); !ok {
+				m.screen = newWordScreen(page)
+				return m, m.screen.Init()
+			}
+		case ld.SearchPage:
+			if _, ok := m.screen.(searchScreen); !ok {
+				m.screen = newSearchScreen()
+				return m, m.screen.Init()
+			}
+		}
+	}
+
+	m.screen, cmd = m.screen.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return m.screen.View()
+}
